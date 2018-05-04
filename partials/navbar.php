b19f4749490ab528e369804a7d6847389b22c396
Eugene Avila <avilaeugeneb@gmail.com>
2018-05-05 07:33:29 +0800

    Fix: Navbar size

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,11 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-dark elegant-color">
+<nav class="navbar navbar-expand-lg navbar-dark elegant-color" id="secondnav">
 
     <!-- Navbar brand -->
     <a class="navbar-brand ml-2" href="#">Pure Food</a>
     <div class="ml-auto d-none d-md-block">
         <ul class="navbar-nav">
-            <a class="navbar-brand white-text">Our Partners:</a>
-
+            <li class="nav-item">
+                <span class="nav-link white-text">Our Partners:</span>
+            </li>
             <li class="nav-item">
                 <a class="nav-link partner-links" href="#">Pure Tools</a>
             </li>
@@ -37,16 +38,16 @@
     <!-- Links -->
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="./home.php">Home</a>
+            <a class="nav-link" href="./home.php#mainnav">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./about.php">About</a>
+            <a class="nav-link" href="./about.php#mainnav">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./catalog.php">Products</a>
+            <a class="nav-link" href="./catalog.php#mainnav">Products</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./cart.php">Cart</a>
+            <a class="nav-link" href="./cart.php#mainnav">Cart</a>
         </li>
     </ul>
     <!-- Links -->
@@ -54,35 +55,33 @@
     <form class="form-inline">
         <div class="md-form mt-0">
             <ul class="navbar-nav mr-auto">
-                <?php 
+                <?php
 
-                if (!isset($_SESSION['user'])){
-                     echo '
-                     <li class="nav-item">
-                     <a class="nav-link" href="./register.php">Register</a>
-                     </li>
-                     <li class="nav-item">
-                     <a class="nav-link" href="./login.php">Log-in</a>
-                     </li>
-                     ';
-                 }
-                 else{
-                   echo '
-                   <li class="nav-item">
-                   <a class="nav-link" href="./profile.php">Profile</a>
-                   </li>
-                   <li class="nav-item">
-                   <a class="nav-link" href="./logout.php">Log-out</a>
-                   </li>
-                   ';
-               }
-
-           ?>
+                    if (!isset($_SESSION['user'])) {
+                        echo '
+                        <li class="nav-item">
+                        <a class="nav-link" href="./register.php#mainnav">Register</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="./login.php#mainnav">Log-in</a>
+                        </li>
+                        ';
+                    } else {
+                        echo '
+                        <li class="nav-item">
+                        <a class="nav-link" href="./profile.php#mainnav">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="./logout.php#mainnav">Log-out</a>
+                        </li>
+                        ';
+                    }
+                ?>
 
 
-       </ul>
+            </ul>
 
-   </div>
-</form>
+        </div>
+    </form>
 </nav>
     <!--/.Navbar-->
