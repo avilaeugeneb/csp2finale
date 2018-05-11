01e017cefbd1770df5dc81a9b727a901eb484ff2
avilaeugeneb <avila.eugeneb@gmail.com>
2018-05-11 09:03:23 +0800

    Feat: Added composer

--- admin/items.php
+++ admin/items.php
@@ -13,22 +13,28 @@ $result_productsqry = mysqli_query($conn,$products_qry);
 
 <div class="grid products">
 	<h3>Products Available</h3>
+
 	<div class="grid tableheader">
-		<div><h4>Product Name</h4></div>
-		<div><h4>Product Description</h4></div>
-		<div><h4>Product Image</h4></div>
-		<div><h4>Product Stocks</h4></div>
-		<div><h4>Product Price</h4></div>
+		<div><b>Product Name</b><i class="fa fa-caret-down" aria-hidden="true"></i></div>
+		<div><b>Product Description</b></div>
+		<div><b>Product Image</b></div>
+		<div><b>Product Stocks</b></div>
+		<div><b>Product Price</b></div>
 	</div>
+
 	<?php while($product = mysqli_fetch_assoc($result_productsqry)) :?>
-		<div class="grid tableheader">
+
+		<div class="grid tableproducts">
 			<div><?= $product['pName']; ?></div>
 			<div><?= $product['pDesc']; ?></div>
 			<div><img src="../assets/img/<?= $product['pImage']; ?>" class="img-fluid"></div>
 			<div><?= $product['pStocks']; ?></div>
 			<div><?= $product['pPrice']; ?></div>
 		</div>
+
 	<?php endwhile; ?>
+
+	<button class="btn btn-info">Add Product</button>
 </div>
 
 
